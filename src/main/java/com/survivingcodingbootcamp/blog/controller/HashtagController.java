c1d8ee5e3464a134635d50b32689cded9cf46123
KayKay-9175 <kellis152@gmail.com>
2022-08-04 15:21:37 -0400

    Mastery Completed

--- src/main/java/com/survivingcodingbootcamp/blog/controller/HashtagController.java
+++ src/main/java/com/survivingcodingbootcamp/blog/controller/HashtagController.java
@@ -2,9 +2,12 @@ package com.survivingcodingbootcamp.blog.controller;
 
 import com.survivingcodingbootcamp.blog.repository.HashtagRepository;
 import com.survivingcodingbootcamp.blog.repository.PostRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/hashtags")
 
 public class HashtagController {
     private HashtagRepository hashtagRepo;
